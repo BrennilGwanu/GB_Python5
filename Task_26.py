87fbfe63144070d46e020d93639b284d8bbc17a5
Maru <121248507+BrennilGwanu@users.noreply.github.com>
2023-07-06 04:06:40 +0500

    Update Task_26.py

--- Task_26.py
+++ Task_26.py
@@ -1,10 +1,10 @@
 #Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
 
-def kvadrat(a, b):
+def degree(a, b):
     if b == 0:
         return(1)
-    return a * kvadrat(a, b - 1)
+    return a * degree(a, b - 1)
 
 num1 = int(input("Введите число: "))
 num2 = int(input("Введите степень: "))
-print(kvadrat(num1, num2))
+print(degree(num1, num2))
